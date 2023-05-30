2473747fbfeda131c18904ec51585416a74c586f
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2023-05-30 20:51:25 +0530

    Create App.js

---
+++ lp2/reacthooks/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+
+import React from 'react';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div>
+      <h1>My React App</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
