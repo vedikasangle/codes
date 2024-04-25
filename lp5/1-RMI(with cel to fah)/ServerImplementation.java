d5b70013338b34ce3202c690bc529bec77fe6e32
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2024-04-25 22:25:29 +0530

    Add files via upload

---
+++ lp5/1-RMI(with cel to fah)/ServerImplementation.java
@@ -0,0 +1,28 @@
+import java.rmi.*;
+import java.rmi.server.*;
+public class ServerImplementation extends UnicastRemoteObject implements 
+ServerInterface{
+public ServerImplementation() throws RemoteException{
+}
+public double Addition(double num1, double num2) 
+throws RemoteException{
+return num1 + num2;
+}
+public double Subtraction(double num1, double num2) 
+throws RemoteException{
+return num1 - num2;
+}
+public double Multiplication(double num1, double num2) 
+throws RemoteException{
+return num1 * num2;
+}
+public double Division(double num1, double num2) 
+throws RemoteException{
+return num1 / num2;
+}
+public double tempConvert(double temCel) 
+throws RemoteException{
+    double f = (tempCel * 9/5) + 32;
+    return f;
+}
+}
